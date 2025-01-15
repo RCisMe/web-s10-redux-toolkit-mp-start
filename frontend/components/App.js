9694d94afd3daeb34e7caea07962874f0d97ab88
R C <126119293+RCisMe@users.noreply.github.com>
2025-01-15 15:13:59 -0500

    Update App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Quotes from './Quotes'
-import QuoteForm from './QuoteForm'
+import React from 'react';
+import Quotes from './components/Quotes';
+import QuoteForm from './components/QuoteForm';
 
 export default function App() {
   return (
     <div id="mp">
-      <h2>Module Project</h2>
-      <Quotes />
-      <QuoteForm />
+      <h2>Module Project - Quotes</h2>
+      <QuoteForm /> {}
+      <Quotes /> {}
     </div>
-  )
+  );
 }
